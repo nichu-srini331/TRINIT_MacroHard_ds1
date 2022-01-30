e27f29962ec2c432b2e3099b3e9ab3bd332d7083
nichu-srini331 <72995669+nichu-srini331@users.noreply.github.com>
2022-01-30 13:19:47 +0530

    Add files via upload

---
+++ practice/webpagebuilder/server.js
@@ -0,0 +1,40 @@
+import express from "express";
+import mongoose from 'mongoose';
+import cors from 'cors';
+import path from 'path';
+import uiRoute from "./ui/ui.rout"
+import pageRoute from "./page/page.route";
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+//SetUp Tempelate engine
+app.use('/resources',express.static(path.join(__dirname, 'public')));
+app.use('views',express.static(path.join(__dirname, 'views')));
+
+// //MONGO DB
+const mongoUri = 'mongodb://localhost:27017/webpage_builder';
+mongoose.connect(
+  mongoUri,
+  {
+    
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log('Connected to MongoDB');
+  },
+);
+
+app.set('view engine', 'hbs');
+
+
+app.use('/pages',pageRoute);
+app.use("/",uiRoute);
+
+const PORT = process.env.PORT || 8085;
+app.listen(PORT,()=>{
+    console.log(`SERVER in ${PORT}`);
+});
